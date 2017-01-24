a8ab4583bc7581f8e05963ff933bb7c9224d142e
Ankit Parekh <ankit.parekh@nyu.edu>
2017-01-24 11:29:33 -0600

    Updated version

--- demo2d.m
+++ demo2d.m
@@ -7,9 +7,11 @@
 % A. Parekh and I. W. Selesnick. Enhanced Low-Rank Matrix Approximation. 
 % IEEE Signal Processing Letters, 23(4):493-497, April 2016. 
 
+%% Initialize
 clear; close all; clc
-%% 2D example
 rng('default');
+%% Load Test Image
+
 S = double(imread('peppers.png'));
 sigma = 30;
 Y = S + sigma * randn(size(S));
@@ -17,23 +19,27 @@ Y = S + sigma * randn(size(S));
 figure(1)
 subplot(1,2,1), imagesc(S), colormap(gray)
 subplot(1,2,2), imagesc(Y), colormap(gray)
-fprintf('Noisy Image PSNR = %2.2f dB \n', csnr(Y,S,0,0))
-%%
-blcksize = [6 6];
-searchSize = [30 30];
-overlap = 4;
+%% Run ELMA method for denoising Test Image
+
+% Parameters 
+blcksize = [8 8];
+searchSize = [36 36];
+overlap = 5;
 threshold = 140;
-lam = 140;
-delta = 0.1;
+lam = 160;
+delta = 0.2;
 is2d = true;
 Est = Y;
+
+% Main iterations
+fprintf('Noisy Image PSNR = %2.2f dB \n', csnr(Y,S,0,0))
 for i = 1:10
     Est = Est + delta * (Y - Est);
     Est = lowRank3D(Est,blcksize,overlap,threshold,searchSize,is2d,lam);
     fprintf('Denoised at step %d. PSNR = %2.2f dB \n', i, csnr(Est,S,0,0))
 end
 
-%%
+%% Plot the estimated image
 figure(1)
 subplot(1,3,1), imshow(uint8(S))
 subplot(1,3,2), imshow(uint8(Y))
